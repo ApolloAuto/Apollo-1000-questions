deada52c34e856080fa12949f7f56cefff4144b2
daohu527 <daohu527@gmail.com>
2024-05-13 11:45:11 +0800

    chore: update sphinx config

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -31,6 +31,7 @@ release = '1.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'myst_parser',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,9 +48,15 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
